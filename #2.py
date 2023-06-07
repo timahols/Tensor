bda1f9917229b33d295a7aa4d5d2777139eb47ac
timahols <117768695+timahols@users.noreply.github.com>
2023-06-08 01:13:46 +0500

    Update #2.py

--- #2.py
+++ #2.py
@@ -9,9 +9,9 @@ class Images:
     def menu_display(self):
         search_bar = wait.until(EC.visibility_of_element_located((By.ID, 'text')))
         search_bar.click()
-        driver.find_element(By.ID, 'text').click()
+        self.driver.find_element(By.ID, 'text').click()
         all_services = driver.find_element(By.CLASS_NAME, 'services-suggest__icons-more')
-        assert all_services.is_displayed(), "Меню не отображается на странице"
+        assert all_services.is_displayed(), "Ошибка: меню не отображается на странице"
         all_services.click()
 
     def transition_images(self):
@@ -22,15 +22,16 @@ class Images:
                 link.click()
                 break
         self.driver.switch_to.window(driver.window_handles[-1])
-        assert self.driver.current_url == 'https://yandex.ru/images/', "Ошибка: не открывался сайт Яндекс.Картинки"
+        assert self.driver.current_url == 'https://yandex.ru/images/', "Ошибка: не удалось перейти на сайт Яндекс.Картинки"
 
     def first_category(self):
-        text_the_first_1 = driver.find_element(By.CLASS_NAME, 'PopularRequestList-SearchText').text
-        driver.find_element(By.CLASS_NAME, 'PopularRequestList-Item_pos_0').click()
+        text_the_first_1 = self.driver.find_element(By.CLASS_NAME, 'PopularRequestList-SearchText').text
+        self.driver.find_element(By.CLASS_NAME, 'PopularRequestList-Item_pos_0').click()
+        assert text_the_first_1, 'Ошибка: первая категория не найдена '
         search_field_result = wait.until(EC.visibility_of_element_located((By.NAME, 'text')))
         search_field_result_2 = search_field_result.get_attribute("value")
-        assert text_the_first_1 == search_field_result_2, 'Ошибка: первая категория не найдена'
-
+        assert text_the_first_1 == search_field_result_2, 'Ошибка: название категории не отображается или ' \
+                                                          'отображается некорректно в поле поиска'
     def open_image(self):
         open_first_picture = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'serp-item__preview')))
         open_first_picture.click()
@@ -39,16 +40,17 @@ class Images:
     def next(self):
         next = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'CircleButton_type_next')))
         global image_element
-        image_element = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+        image_element = self.driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+        assert image_element, 'Ошибка: Картинка не открылась'
         next.click()
-        new_image_element = driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
+        new_image_element = self.driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute('src')
         assert image_element != new_image_element, "Ошибка: картинка не поменялась"
 
     def back(self):
         back = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.CircleButton_type_prev')))
         back.click()
         assert image_element == self.driver.find_element(By.CLASS_NAME, "MMImage-Origin").get_attribute(
-            'src'), "Ошибка: картинка не вернулась"
+            'src'), "Ошибка: картинка не вернулась в исходную из 8 пункта"
 
 
 def test_tesnor_search():
